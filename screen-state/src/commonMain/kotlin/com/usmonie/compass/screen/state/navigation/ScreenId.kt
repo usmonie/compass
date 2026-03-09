035c57e4de0b636b9850eb20906ab513e7332467
Usman Akhmedov <akhme.v@gmail.com>
2026-03-09 14:53:14 +0100

    Added our own map lib Renamed MapSearchScreen to PropertyExplorerScreen Improvements with filters

--- screen-state/src/commonMain/kotlin/com/usmonie/compass/screen/state/navigation/ScreenId.kt
+++ screen-state/src/commonMain/kotlin/com/usmonie/compass/screen/state/navigation/ScreenId.kt
@@ -10,7 +10,26 @@ import kotlinx.serialization.Serializable
 public open class ScreenId(
     public val id: String,
     public open val mode: Mode = Mode.STANDARD,
-) : Extra, NavKey
+) : Extra, NavKey {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as ScreenId
+
+        if (id != other.id) return false
+        if (mode != other.mode) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        Exception().printStackTrace()
+        var result = id.hashCode()
+        result = 31 * result + mode.hashCode()
+        return result
+    }
+}
 
 @Serializable
 public enum class Mode {
